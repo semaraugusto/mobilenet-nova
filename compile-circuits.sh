a6a2ef9ab726588158dbd46b493748d1d8bf46c3
semar <semaraugusto@gmail.com>
2023-12-24 18:19:47 -0300

    add DEBUG flag to compile-circuits.sh

--- compile-circuits.sh
+++ compile-circuits.sh
@@ -1,5 +1,6 @@
 source ./circuits/compile.sh;
 PTAU_PATH=./powersOfTau28_hez_final_24.ptau;
+DEBUG=true;
 
 echo "starting circuit compilation"
 
@@ -10,13 +11,18 @@ echo "npm install done" &&
 
 compile head bn128 &&
 echo "head compilation done" &&
-compile_phase2 head &&
-echo "head phase2 done" &&
+if [ ! DEBUG ]; then
+	compile_phase2 head && # This is very slow and requires a lot of RAM.
+	echo "head phase2 done"
+fi
 
 compile tail bn128 &&
 echo "tail compilation done" &&
-compile_phase2 tail &&
-echo "tail phase2 done!" &&
+
+if [ ! DEBUG ]; then
+	compile_phase2 tail && # This is very slow and requires a lot of RAM
+	echo "tail phase2 done!"
+fi
 #
 compile backbone vesta &&
 echo -e "backbone done!\n" &&
